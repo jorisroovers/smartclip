6d79b096a2ad42f9ac8dcdbb8829dfa21274c5bd
Joris Roovers <joris.roovers@gmail.com>
2017-02-11 12:38:38 +0100

    Initial electron boilerplate code
    README.md, package.json, main.js and a test image!

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# smartclip
-Smart clipboard for macOS
+# Smartclip #
+
+Smart clipboard app for macOS written in NodeJS using electron. Focus on developer and power-user convenience.
+
+I have some good ideas, but it's very early stages at this point! Using this opportunity to learn ReactJS along the way :-)
+
+# TODO #
+- Storing clips in encrypted file
+    - Generate encryption key on first install (this is for later)
+- image resizing and cropping from tray
+- autoformat json, yaml, etc
+- convert from json to e.g. yaml
+- trim whitespace
+- base64, md5, encode, etc
---
+++ smartclip/main.js
@@ -0,0 +1,83 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+    console.log("Creating window!");
+
+
+    mainWindow = new BrowserWindow({width: 1000, height: 800});
+    // mainWindow.loadURL(`file://${__dirname}/views/index.html`);
+
+    // We can access settings here like this:
+    // const storage = require('electron-json-storage');
+    // storage.get('settings.user', function (error, data) {
+    //     // console.log(data);
+    // });
+
+    mainWindow.on('close', function (event) {
+        // if we're not really quitting, then just hide the window
+        if (!app.isQuitting) {
+            event.preventDefault();
+            mainWindow.hide();
+            return false;
+        }
+    });
+
+}
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function () {
+    app.isQuitting = false;
+    createWindow();
+});
+
+
+// Quit when all windows are closed.s
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('before-quit', function () {
+    console.log("Quiting!");
+    app.isQuitting = true;
+});
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    } else {
+        mainWindow.show();
+    }
+});
+
+
+const clipboardWatcher = require('electron-clipboard-watcher')
+clipboardWatcher({
+    // (optional) delay in ms between polls
+    watchDelay: 1000,
+    // handler for when image data is copied into the clipboard
+    onImageChange: function (nativeImage) {
+        console.log("Image copied");
+        console.log(nativeImage);
+
+    },
+    // handler for when text data is copied into the clipboard
+    onTextChange: function (text) {
+        console.log("text copied");
+        console.log(text);
+    }
+});
